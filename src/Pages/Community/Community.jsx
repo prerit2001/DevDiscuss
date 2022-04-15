33ba7386da6036cf3a1ac7c568acdc61a6deb4c5
Prerit Kumar Jha <preritkrjha@gmail.com>
2022-04-15 23:03:43 +0530

    Communities Page

--- src/Pages/Community/Community.jsx
+++ src/Pages/Community/Community.jsx
@@ -1,25 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./css/Community.css";
 import TextField from "@mui/material/TextField";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import { makeStyles } from "@material-ui/core/styles";
+import { CommunityCard } from "./CommunityCard";
+import axios from "axios";
+import Typewriter from "typewriter-effect";
+
+const useStyles = makeStyles({
+  CommunityCardCover: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    margin: "auto",
+    flexWrap: "wrap",
+  },
+});
 
 export const Community = () => {
   const [age, setAge] = React.useState(10);
+  const [Community, setCommunity] = useState({ data: [] });
 
   const handleChange = (event) => {
     setAge(event.target.value);
   };
 
-  // const prerit = "texas";
+  const classes = useStyles();
+  const { CommunityCardCover } = classes;
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/community")
+      .then((data) => {
+        setCommunity(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div style={{ textAlign: "center" }}>
-      {/* {prerit.last.name} */}
       <div className="BGimage">
-        <div style={{ padding: "2px" }}>Explore Communities</div>
+        <div style={{ padding: "2px" }}>
+          Explore{" "}
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Communities")
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString("#tags")
+                .pauseFor(2000)
+                .deleteAll()
+                .typeString("Threads")
+                .start();
+            }}
+          />
+        </div>
       </div>
       <div className="searchBox">
         <div style={{ display: "flex", margin: "10px" }}>
@@ -61,6 +103,11 @@ export const Community = () => {
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHl_Awxa0w_YWe3chZKuToRT9du5eVjwpbNA&usqp=CAU"
           />
         </div>
+      </div>
+      <div className={CommunityCardCover}>
+        {Community.data.map((community) => {
+          return <CommunityCard {...community} />;
+        })}
       </div>
     </div>
   );
